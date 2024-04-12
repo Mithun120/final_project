b63ab075742193ffea98bf082b422c72b93ccfa2
Mithun M <mithunm@jmangroup.com>
2024-04-12 17:10:59 +0530

    power bi

--- client/src/data/powerbi.jsx
+++ client/src/data/powerbi.jsx
@@ -3,8 +3,7 @@ import React from 'react'
 const powerbi = () => {
   return (
     <>
-      <iframe title="final_project" width="100%" height="641.25" src="https://app.powerbi.com/reportEmbed?reportId=c520cf3d-d7cf-472a-91bd-931a5c966308&autoAuth=true&ctid=2800c0a0-70e9-49be-8733-faeaa6aced99" frameborder="0" allowFullScreen="true"></iframe>
-    </>
+<iframe title="final_project" width="100%" height="641.25"   src="https://app.powerbi.com/reportEmbed?reportId=c520cf3d-d7cf-472a-91bd-931a5c966308&autoAuth=true&ctid=2800c0a0-70e9-49be-8733-faeaa6aced99" frameborder="0" allowFullScreen="true"></iframe>    </>
   )
 }
 
